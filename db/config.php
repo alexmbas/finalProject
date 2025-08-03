cabd598a8ddf596b7dd0a9e41c9a45e8303988f2
alexmbas <97928275+alexmbas@users.noreply.github.com>
2025-08-03 12:44:00 -0500

    Switched DB config to use JAWSDB from Heroku

--- db/config.php
+++ db/config.php
@@ -1,19 +1,25 @@
 <?php
-// Load Composer's autoloader
-require_once __DIR__ . '/../vendor/autoload.php';
+if (getenv('JAWSDB_URL')) {
+    // Using Heroku's' JAWSDB_URL
+    $url = parse_url(getenv("JAWSDB_URL"));
 
-// Load environment variables from .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+    $host = $url["host"];
+    $db   = substr($url["path"], 1);
+    $user = $url["user"];
+    $pass = $url["pass"];
+} else {
+    // Retain local compatibilities, load from .env
+    require_once __DIR__ . '/../vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
 
-// Database credentials from .env
-$host = $_ENV['DB_HOST'];
-$db   = $_ENV['DB_NAME'];
-$user = $_ENV['DB_USER'];
-$pass = $_ENV['DB_PASS'];
+    $host = $_ENV['DB_HOST'];
+    $db   = $_ENV['DB_NAME'];
+    $user = $_ENV['DB_USER'];
+    $pass = $_ENV['DB_PASS'];
+}
+
 $charset = 'utf8mb4';
-
-// PDO DSN and options
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
